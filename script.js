4ef35fd884a005ff5f5c93f5e0ddea1a2c57bc95
Flavio <flavioezequiela@gmail.com>
2024-05-20 22:04:37 -0300

    adicionando o outro campo

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-var divBox = document.getElementById('box');
-
+var classe = ['caixa', 'box'];
 var linhasEcolunas = 10;
 
-for(var i = 0; i < linhasEcolunas; i++){
-    var coluna = document.createElement('div');
-    coluna.setAttribute('class', 'colunas');
-    divBox.appendChild(coluna);
+for(var a = 0; a < classe.length; a++){
+    var divBox = document.getElementById(classe[a]);
 
-    for(var j = 0; j < linhasEcolunas; j++){
-        var linhas = document.createElement('div');
+    for(var i = 0; i < linhasEcolunas; i++){
+        var coluna = document.createElement('div');
+        coluna.setAttribute('class', 'colunas');
+        divBox.appendChild(coluna);
 
-        linhas.setAttribute('class', 'linhas');
-        coluna.appendChild(linhas);
-        //console.log("porraaaaaaaaaa");
+        for(var j = 0; j < linhasEcolunas; j++){
+            var linhas = document.createElement('div');
+
+            linhas.setAttribute('class', 'linhas');
+            coluna.appendChild(linhas);
+            //console.log("porraaaaaaaaaa");
+        }
+
+
     }
-
-
 }
